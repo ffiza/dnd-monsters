b44acc481fed6fb12d7f9105ba0466077ead976a
Federico Iza <fedegiza@gmail.com>
2025-04-06 16:59:51 -0300

    test

--- src/app.py
+++ src/app.py
@@ -48,4 +48,4 @@ app.layout = dbc.Container([
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
