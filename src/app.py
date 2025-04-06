819f26641505095ed4e18d1d3fbe1de2ad1d235c
Federico Iza <fedegiza@gmail.com>
2025-04-06 16:50:24 -0300

    test commit

---
+++ src/app.py
@@ -0,0 +1,49 @@
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
+import plotly.express as px
+
+from data import generate_dummy_data
+
+df = generate_dummy_data(n_data=50)
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+
+# Graphs
+fig = px.scatter(df, x="Strength", y="Dexterity")
+
+# App layout
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H1("Dungeons & Dragons 5th Edition Monsters"),
+                style={"textAlign":  "center",
+                       "fontSize": 24},
+                width={"size": 8, "offset": 2})
+    ]),
+    dbc.Row([
+        dbc.Col(html.P("Text description"),
+                style={"textAlign":  "left",
+                       "fontSize": 18},
+                width={"size": 8, "offset": 2})
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(figure=fig),
+                width=12)
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(figure=fig),
+                width=4),
+        dbc.Col(dcc.Graph(figure=fig),
+                width=4),
+        dbc.Col([
+            dbc.Row(html.H4("Creature Name"),
+                    style={"textAlign": "center"}),
+            dbc.Row(dcc.Graph(figure=fig)),
+            dbc.Row(dcc.Graph(figure=fig)),
+            dbc.Row(dcc.Graph(figure=fig)),
+        ], width=4)
+    ])
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/data.py
@@ -0,0 +1,195 @@
+import pandas as pd
+import numpy as np
+import json
+
+
+def generate_dummy_data(n_data: int) -> pd.DataFrame:
+    CHALLENGE_RATINGS = ["0", "1/8", "1/4", "1/2", "1", "2", "3", "4", "5",
+                         "6", "7", "8", "9", "10", "11", "12", "13", "14",
+                         "15", "16", "17", "18", "19", "20", "21", "22", "23",
+                         "24", "25", "26", "27", "28", "29", "30"]
+    TYPES = ["Aberration", "Beast", "Celestial", "Construct", "Dragon",
+             "Elemental", "Fey", "Fiend", "Giant", "Humanoid", "Monstrosity",
+             "Ooze", "Plant", "Undead"]
+    SIZES = ["Tiny", "Small", "Medium", "Large", "Huge", "Gargantuan"]
+    ALIGNMENTS = ["LG", "NG", "CG", "LN", "N", "CN", "LE", "NE", "CE", "U"]
+
+    df = pd.DataFrame({
+        "Name": [f"Monster{i}" for i in range(n_data)],
+        "ChallengeRating": np.random.choice(CHALLENGE_RATINGS, n_data),
+        "Type": np.random.choice(TYPES, n_data),
+        "Size": np.random.choice(SIZES, n_data),
+        "Strength": np.random.randint(1, 21, n_data),
+        "Dexterity": np.random.randint(1, 21, n_data),
+        "Constitution": np.random.randint(1, 21, n_data),
+        "Intelligence": np.random.randint(1, 21, n_data),
+        "Wisdom": np.random.randint(1, 21, n_data),
+        "Charisma": np.random.randint(1, 21, n_data),
+        "Alignment": np.random.choice(ALIGNMENTS, n_data),
+        "Speed": np.random.randint(20, 41, n_data),
+        "SwimSpeed": np.random.randint(0, 41, n_data),
+        "FlySpeed": np.random.randint(0, 41, n_data),
+    })
+
+    return df
+
+
+def parse_speed_line(s: str) -> dict:
+    """
+    Parses the `Speed` string in a monster block.
+
+    Parameters
+    ----------
+    s : str
+        The `Speed` string of the monster block.
+
+    Returns
+    -------
+    dict
+        A dictionary with all the speeds of the monster.
+    """
+    data = {"walk": 0, "swim": 0, "fly": 0, "burrow": 0, "climb": 0}
+    s_split = s.split(".")
+    for e in s_split:
+        if "ft" in e:
+            if "swim" in e:
+                data["swim"] = e.split(" ")[2]
+            elif "fly" in e:
+                data["fly"] = e.split(" ")[2]
+            elif "burrow" in e:
+                data["burrow"] = e.split(" ")[2]
+            elif "climb" in e:
+                data["climb"] = e.split(" ")[2]
+            else:
+                data["walk"] = e.split(" ")[0]
+    return data
+
+
+def parse_monster_block(block: dict) -> list:
+    """
+    Parses the monster block.
+
+    Parameters
+    ----------
+    block : dict
+        The block to parse.
+
+    Returns
+    -------
+    list
+        A list with relevant values.
+    """
+    speed_data = parse_speed_line(block["Speed"])
+    if block["name"] == "Werebear":
+        data = [block["name"],
+                block["Challenge"].split(" (")[0],
+                block["meta"].split(" ")[1].split(",")[0],
+                block["meta"].split(" ")[0],
+                int(block["STR"]),
+                int(block["DEX"]),
+                int(block["CON"]),
+                int(block["INT"]),
+                int(block["WIS"]),
+                int(block["CHA"]),
+                block["meta"].split(", ")[1],
+                int(block["Speed"][8:10]),
+                0,
+                0,
+                0,
+                int(block["Speed"][22:24]),
+                ]
+    elif block["name"] == "Wereboar":
+        data = [block["name"],
+                block["Challenge"].split(" (")[0],
+                block["meta"].split(" ")[1].split(",")[0],
+                block["meta"].split(" ")[0],
+                int(block["STR"]),
+                int(block["DEX"]),
+                int(block["CON"]),
+                int(block["INT"]),
+                int(block["WIS"]),
+                int(block["CHA"]),
+                block["meta"].split(", ")[1],
+                int(block["Speed"][8:10]),
+                0,
+                0,
+                0,
+                0,
+                ]
+    elif block["name"] == "Weretiger":
+        data = [block["name"],
+                block["Challenge"].split(" (")[0],
+                block["meta"].split(" ")[1].split(",")[0],
+                block["meta"].split(" ")[0],
+                int(block["STR"]),
+                int(block["DEX"]),
+                int(block["CON"]),
+                int(block["INT"]),
+                int(block["WIS"]),
+                int(block["CHA"]),
+                block["meta"].split(", ")[1],
+                int(block["Speed"][8:10]),
+                0,
+                0,
+                0,
+                0,
+                ]
+    elif block["name"] == "Werewolf":
+        data = [block["name"],
+                block["Challenge"].split(" (")[0],
+                block["meta"].split(" ")[1].split(",")[0],
+                block["meta"].split(" ")[0],
+                int(block["STR"]),
+                int(block["DEX"]),
+                int(block["CON"]),
+                int(block["INT"]),
+                int(block["WIS"]),
+                int(block["CHA"]),
+                block["meta"].split(", ")[1],
+                int(block["Speed"][8:10]),
+                0,
+                0,
+                0,
+                0,
+                ]
+    else:
+        data = [block["name"],
+                block["Challenge"].split(" (")[0],
+                block["meta"].split(" ")[1].split(",")[0],
+                block["meta"].split(" ")[0],
+                int(block["STR"]),
+                int(block["DEX"]),
+                int(block["CON"]),
+                int(block["INT"]),
+                int(block["WIS"]),
+                int(block["CHA"]),
+                block["meta"].split(", ")[1],
+                int(speed_data["walk"]),
+                int(speed_data["swim"]),
+                int(speed_data["fly"]),
+                int(speed_data["burrow"]),
+                int(speed_data["climb"]),
+                ]
+    return data
+
+
+def process_data() -> pd.DataFrame:
+    with open('data/raw/srd_5e_monsters.json', 'r') as file:
+        monsters = json.load(file)
+
+    cols = ["Name", "ChallengeRating", "Type", "Size", "Strength", "Dexterity",
+            "Constitution", "Intelligence", "Wisdom", "Charisma", "Alignment",
+            "WalkSpeed", "SwimSpeed", "FlySpeed", "BurrowSpeed", "ClimbSpeed"]
+    data = []
+    for monster in monsters:
+        data.append(parse_monster_block(monster))
+    data = np.array(data)
+
+    df = pd.DataFrame(data, columns=cols)
+    df["Type"] = df["Type"].str.title()
+
+    df.to_csv("data/processed/srd_5e_monsters.csv", index=False)
+
+
+if __name__ == "__main__":
+    process_data()
